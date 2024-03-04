79bd723c985cbadae90dd3213041badd4c11f834
GirishCodeAlchemy <venkatareddygirish@gmail.com>
2024-03-04 10:12:31 -0500

    Added various for loop

---
+++ nodejs/for_loop.js
@@ -0,0 +1,22 @@
+// Traditional for loop:
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
+
+// for...of loop (for iterating over arrays or iterable objects):
+const array = [1, 2, 3, 4, 5];
+for (const element of array) {
+    console.log(element);
+}
+
+// for...in loop (for iterating over object properties):
+const obj = { a: 1, b: 2, c: 3 };
+for (const key in obj) {
+    console.log(key, obj[key]);
+}
+
+// for each loop (for iterating over arrays):
+const array1 = [1, 2, 3, 4, 5];
+array1.forEach((element, index) => {
+    console.log(`Element at index ${index}: ${element}`);
+});
